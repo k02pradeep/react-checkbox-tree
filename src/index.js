8383edc57484e495b819a3cfea7613a1530dc726
Pradeep K <k02pradeep@gmail.com>
2023-06-20 18:28:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import CheckboxTree from './js/CheckboxTree';
 import TreeNode from './js/TreeNode';
+
 export default CheckboxTree;
+export {
+    CheckboxTree,
+    TreeNode
+}
 export * from './js/utils';
